d42607e4163a99b47805b8dcdc0e3c3bd85218d5
Louis Kuang <louiskuang@Kiras-MacBook-Pro-2.local>
2023-06-25 17:00:18 -0400

    Add incorrect implementation of mixed numeric comparison.

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,18 @@ impl From<serde_json::Number> for FsNumber {
     }
 }
 
+fn cmp_i64_f64(left: i64, right: f64) -> Ordering {
+    // TODO(louiskuang): this cast can lose precision
+    let left_as_f64: f64 = left as f64;
+    left_as_f64.total_cmp(&right)
+}
+
+fn cmp_u64_f64(left: u64, right: f64) -> Ordering {
+    // TODO(louiskuang): this cast can lose precision
+    let left_as_f64: f64 = left as f64;
+    left_as_f64.total_cmp(&right)
+}
+
 impl Ord for FsNumber {
     fn cmp(&self, other: &Self) -> Ordering {
         if self.eq(other) {
@@ -31,7 +43,47 @@ impl Ord for FsNumber {
             (FsNumber::Number(_), FsNumber::PositiveInfinity) => Ordering::Less,
             (FsNumber::Number(_), FsNumber::NegativeInfinity) => Ordering::Greater,
             (FsNumber::Number(left), FsNumber::Number(right)) => {
-                panic!("TODO(louiskuang): implement mixed integer and floating point number comparison");
+                if left.is_i64() {
+                    let left_as_i64 = left.as_i64().unwrap();
+                    if right.is_i64() {
+                        return left_as_i64.cmp(&right.as_i64().unwrap());
+                    } else if right.is_u64() {
+                        let right_as_u64 = right.as_u64().unwrap();
+                        let right_as_i64: Result<i64, _> = right_as_u64.try_into();
+                        return match right_as_i64 {
+                            Ok(value) => left_as_i64.cmp(&value),
+                            Err(_) => Ordering::Less,
+                        };
+                    } else if right.is_f64() {
+                        return cmp_i64_f64(left_as_i64, right.as_f64().unwrap());
+                    }
+                    panic!("impossible")
+                } else if left.is_u64() {
+                    let left_as_u64 = left.as_u64().unwrap();
+                    if right.is_u64() {
+                        return left_as_u64.cmp(&right.as_u64().unwrap());
+                    } else if right.is_i64() {
+                        let right_as_i64 = right.as_i64().unwrap();
+                        let left_as_i64: Result<i64, _> = left_as_u64.try_into();
+                        return match left_as_i64 {
+                            Ok(value) => value.cmp(&right_as_i64),
+                            Err(_) => Ordering::Greater,
+                        };
+                    } else if right.is_f64() {
+                        return cmp_u64_f64(left_as_u64, right.as_f64().unwrap());
+                    }
+                    panic!("impossible")
+                } else if left.is_f64() {
+                    let left_as_f64 = left.as_f64().unwrap();
+                    if right.is_f64() {
+                        return left_as_f64.total_cmp(&right.as_f64().unwrap());
+                    } else if right.is_i64() {
+                        return cmp_i64_f64(right.as_i64().unwrap(), left_as_f64).reverse();
+                    } else if right.is_u64() {
+                        return cmp_u64_f64(right.as_u64().unwrap(), left_as_f64).reverse();
+                    }
+                }
+                panic!("impossible")
             }
         }
     }
